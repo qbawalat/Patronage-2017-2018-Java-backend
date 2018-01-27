59f4cf45ce2ffe1875b9fa7843e7b6e311a392c1
stoicfey <kubawalat@gmail.com>
2018-01-27 17:34:47 +0100

    Fixed swagger; added request/response and http responses. TODO: identification of http error's cause

--- src/main/java/org/kwalat/patronage/model/Car.java
+++ src/main/java/org/kwalat/patronage/model/Car.java
@@ -2,6 +2,7 @@ package org.kwalat.patronage.model;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import org.kwalat.patronage.validators.date.InDateRange;
 import org.kwalat.patronage.validators.date.ValidCarRegistrationDate;
@@ -19,35 +20,48 @@ import java.util.Date;
 @ValidCarRegistrationDate
 @Entity
 @XmlRootElement
-public class Car {
 
+public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty(required = true)
+    @ApiModelProperty(value = "Represents id of car entity", required = true)
     private Long id;
     @NotNull
     @Min(50)
     @Max(6999)
+    @JsonProperty(required = true)
+    @ApiModelProperty(value = "Represents car's engine size", required = true)
     private Integer engineSize;
     @NotNull
+    @JsonProperty(required = true)
+    @ApiModelProperty(value = "Represents car's brand", required = true)
     private Brand brand;
     @Min(1)
     @Max(6)
+    @JsonProperty(required = true)
+    @ApiModelProperty(value = "Represents the number of seats in the car", required = true)
     private Byte seats;
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @InDateRange("1900-01-01")
     @Past
+    @JsonProperty(required = true)
+    @ApiModelProperty(value = "Represents the first registration date of car's registration book ", required = true)
     private Date firstRegistrationDate;
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Past
+    @JsonProperty(required = true)
+    @ApiModelProperty(value = "Represents the date of issue of registration book", required = true)
     private Date registrationBookDateOfIssue;
     @NotNull
     @Size(max = 10)
     @LegitLicence()
-    @ApiModelProperty(notes = "Provided car licence number", required = true)
+    @JsonProperty(required = true)
+    @ApiModelProperty(value = "Represents car's licence number", required = true)
     private String licenceNumber;
 
     public Car(Integer engineSize, Brand brand, Byte seats, Date firstRegistrationDate, Date registrationBookDateOfIssue, String licenceNumber) {
